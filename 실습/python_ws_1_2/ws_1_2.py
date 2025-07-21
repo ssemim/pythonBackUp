9f228d870569cf773ec9e6ae57813bfcdd29b1b9
HyoRyeon(SSEMIM) <82812894+ssemim@users.noreply.github.com>
2025-07-21 17:02:31 +0900

    Add files via upload

---
+++ 실습/python_ws_1_2/ws_1_2.py
@@ -0,0 +1,19 @@
+string = '문자열'
+integer = 10
+floating_point = 3.14
+a_list = [1, 2, 3, 4, 5]
+dictionary = {'name': '홍길동', 'age': 20}
+a_set = {1, 2, 3, 4, 5}
+a_range = range(11)
+a_tuple = (1, 2, 3)
+boolean = True
+
+print(type(string))          # <class 'str'>
+print(type(integer))        # <class 'int'> 
+print(type(floating_point)) # <class 'float'>
+print(type(a_list))        # <class 'list'>
+print(type(dictionary))     # <class 'dict'>
+print(type(a_set))         # <class 'set'>
+print(type(a_range))       # <class 'range'>
+print(type(a_tuple))       # <class 'tuple'>
+print(type(boolean))       # <class 'bool'>
