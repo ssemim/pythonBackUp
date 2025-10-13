82854a57fbc73f0a178a8f1092df3d8f3be0f8ba
HyoRyeon(SSEMIM) <82812894+ssemim@users.noreply.github.com>
2025-10-14 08:53:15 +0900

    Add files via upload

---
+++ todos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    work = models.CharField(max_length=100)
+    content = models.TextField()
+    is_completed = models.BooleanField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.work} : {self.content}"
+    
+    def __repr__(self):
+        return super().__repr__()
