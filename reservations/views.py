bbbd693565e213efa5fa3edced6570651e846d49
HyoRyeon(SSEMIM) <82812894+ssemim@users.noreply.github.com>
2025-10-14 08:52:08 +0900

    Add files via upload

---
+++ reservations/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Reservation
+
+# Create your views here.
+def index(request):
+    reservations = Reservation.objects.all()
+    context = {
+        'reservations': reservations
+    }
+    return render(request, 'reservations/index.html', context)
